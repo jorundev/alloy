1eb2ae7eacab889887cfcc6d4417d7fb189bd9b7
jorun <contact@jorun.dev>
2023-03-13 00:14:33 +0100

    Added better documentation

--- STRUCTURE.md
+++ STRUCTURE.md
@@ -9,15 +9,20 @@ The project is separated in two parts:
 
 The root `Cargo.toml` defines a [workspace](https://doc.rust-lang.org/book/ch14-03-cargo-workspaces.html) so that every single project crate uses the same `target/` directory.
 
-
 ## bootcore
 
 `bootcore` is the part of the code that is executed before `kernel_main`. It is responsible of:
 - Getting information from the bootloader, translating it into a format that the kernel can understand
 - Map the kernel to the higher half of the address space
 - Map the framebuffer / text buffer (if there is one)
-- Jump to `kernel_main` (in the higher half)
+- Jump to `kernel_main_stub` (in the higher half)
 
 This crate is situated in the `bootcore/` directory.
 
 It is built into a static library. All the sections of that library then gets linked into the main kernel ELF in the `.boot.*` sections. This is done so that all of the bootcore's code is in the lower half of the address space. bootcore also has its own panic handler as a result.
+
+The main `build.rs` build script waits for bootcore to be compiled before linking to it
+
+## memory
+
+`memory` is the crate used to interact with memory (paging...). It is used by bootcore as well as the main kernel
--- build.rs
+++ build.rs
@@ -1,6 +1,11 @@
 use std::time::Duration;
 
+/*
+	Cargo allows to build staticlib crates but not linking to them.
+	This build script waits for the library to be built, then links to it.
+ */
 fn main() {
+	// Passed from Makefile. Directory where the dependencies are built
     let bootcore_dir = std::env::var("LIBBOOTCORE_DIR").unwrap();
     let mut bootcore = None;
 
@@ -9,6 +14,9 @@ fn main() {
         for path in paths {
             let str = String::from(path.unwrap().path().to_str().unwrap());
             let start = format!("{}/libbootcore", bootcore_dir);
+
+			// Check if there is a file starting with "$LIBBOOTCORE_DIR/libbootcore", ending with ".a"
+			// and not containing "temp"
             if str.starts_with(&start) && str.ends_with(".a") && !str.contains("temp") {
                 let final_str = &str[(bootcore_dir.len() + 4)..];
                 let final_str = &final_str[..(final_str.len() - 2)];
@@ -25,11 +33,12 @@ fn main() {
         std::thread::sleep(Duration::from_millis(100));
     }
 
-    if bootcore.is_none() {
-        panic!("Didn't find libbootcore");
-    }
+	// If we are out of the loop then bootcore is not None
+	let bootcore = bootcore.unwrap();
 
+	// TODO: depends on target
     println!("cargo:rerun-if-changed=layouts/x86.ld");
+
     println!("cargo:rustc-link-search={bootcore_dir}");
-    println!("cargo:rustc-link-lib={}", bootcore.unwrap());
+    println!("cargo:rustc-link-lib={}", bootcore);
 }
